324923e771bbc8c5f8ce672063ef256260b2f758
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-09-12 03:36:07 +0530

    introduction data type php

--- app1/index.php
+++ app1/index.php
@@ -3,10 +3,20 @@
 /* echo "<h1>Headers</h1>";
 print("<h1>Headers</h1>"); */
 
-//variables
-$name='Nimal';
-$Name='Saman';
-$myName='saman pradeep';
-print($name);
+// --------<<< variables
+// $name='Nimal';
+// $Name='Saman';
+// $myName='saman pradeep';
+// print($name);
+
+// -------<<< Data Type
+// int
+$age = 50;
+// double
+$salary =10000.50;
+// string
+$name= 'amal';
+// boolean
+$isExists=true;
 
 ?>
