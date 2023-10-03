d541cc8de45cf8ad69e6e9500f583b652d8822af
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-10-03 22:20:10 +0530

    php elseif condition introduced

--- app1/index2.php
+++ app1/index2.php
@@ -1,8 +1,26 @@
 <?php>
 
-/*  ---------- if  ------------- */
+/*
+   ---------- if  -------------
 $age=30;
 if($age>=18){
 echo "You are an adult !";
+}else{
+echo "You are not eligible ";
 }
+ */
+
+$marks=80;
+
+if($marks>=90){
+    echo "A";
+}elseif($marks>=80){
+    echo "B"
+}elseif($marks>=70){
+    echo "C";
+}else{
+    echo "F";
+}
+
+
 ?>
